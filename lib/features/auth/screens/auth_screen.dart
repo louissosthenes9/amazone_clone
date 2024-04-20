9ddf93b29cb7ba0210a12ea5e86cce825026de3f
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-20 21:26:13 +0300

    fixed routes

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,35 +1,108 @@
+import 'package:amazon_clone/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
+enum Auth {
+  signin,
+  signup
+}
+
 class AuthScreen extends StatefulWidget {
-  const AuthScreen({super.key});
+  const AuthScreen({Key? key}) : super(key: key);
+
   @override
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signin;
+  final _signUpFormKey = GlobalKey<FormState>();
+  final _signInFormKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: GlobalVariables.greyBackgroundCOlor,
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(
-              'Welcome',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w500,
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                'Welcome',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            ListTile(
-              title: Text("create account"),
-            )
-          ],
+              ListTile(
+                title: const Text(
+                  "create account",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signup,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(() {
+                      _auth = val!;
+                    });
+                  },
+                ),
+              ),
+              if(_auth ==Auth.signup)
+               Container(
+                 color: GlobalVariables.backgroundColor,
+                 padding:const EdgeInsets.all(8),
+                 child: Form(
+                   key:_signUpFormKey,
+                   child:Column(
+                       children: [
+                         CustomTextField(
+                           controller: _nameController,
+                           hintText: "name",
+                         ),
+                         CustomTextField(
+                           controller: _emailController,
+                           hintText: "Email",
+                         ),
+                         CustomTextField(
+                           controller: _passwordController,
+                           hintText: "password",
+                         ),
+
+                       ]
+                   ),
+                 ),
+               ),
+
+              ListTile(
+                title: const Text(
+                  "Sign in",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signin,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(() {
+                      _auth = val!;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
