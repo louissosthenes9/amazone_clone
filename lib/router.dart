24aa9b4ebd22fa6d707ec833493167526fffbb76
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-20 18:34:30 +0300

    fixed routes

--- lib/router.dart
+++ lib/router.dart
@@ -1,18 +1,26 @@
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 
-Route<dynamic> generateRoute(RouteSettings routeSettings) {
-  switch (routeSettings.name) {
-    case AuthScreen.routeName:
-      return MaterialPageRoute(
-        settings: routeSettings,
-        builder: (_) => const AuthScreen(),
-      );
-    default:
-      return MaterialPageRoute(
-          settings: routeSettings,
-          builder: (_) => const Scaffold(
-                body: Center(child: Text("the screen doesn't exist")),
-              ));
+class RouteGenerator{
+  static const String authScreen = "/authScreen";
+  static const String randomPage = "/random";
+  static const String homePage = "/";
+  static Route<dynamic> generateRoute(RouteSettings routeSettings) {
+
+
+    switch (routeSettings.name) {
+      case authScreen:
+        return MaterialPageRoute(
+          builder: (_) => const AuthScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+            settings: routeSettings,
+            builder: (_) => const Scaffold(
+              body: Center(child: Text("the screen doesn't exist")),
+            ));
+    }
   }
+
 }
+
