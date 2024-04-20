a9808fd4894d2215fab7f04de70aaaa00901b948
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-20 18:34:50 +0300

    fixed routes

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
-  static const String routeName = '/auth-screen';
   @override
   State<AuthScreen> createState() => _AuthScreenState();
 }
