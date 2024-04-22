8046c0478e81daca72d3c3944116b0e7711b149f
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-22 14:28:07 +0300

    fixed the authrouter bug

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const authrouter = require('./routes/auth')
+
 const app = express()
 const PORT = 3000;
-app.use(authrouter);
-app.listen(PORT,"0.0.0.0",()=>{
 
+const { authRouter } = require('./routes/auth')
+app.use('/',authRouter)
+
+app.listen(PORT,"0.0.0.0",()=>{
+   console.log(`connected to ${PORT}`)
 })
