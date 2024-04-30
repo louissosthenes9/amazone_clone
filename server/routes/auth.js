1951df891b2010478c075324bfb3ed4213637b75
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-01 01:16:04 +0300

    added logic to signup endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,30 @@
 const express = require("express")
-
+const User = require('../models/User')
 const authRouter = express.Router()
-authRouter.get('/user',(req,res)=>{
-  res.json({"name":"Louis"})
-})
-
-authRouter.post('/api/signup',(req,res)=>{
+authRouter.post('/api/signup',async (req,res)=>{
     const {name,email,password}= req.body;
+   try{
+       const existingUser = await User.findOne({email})
+       if (existingUser){
+           return res
+               .status(400)
+               .json({message:"User with the same email already exists!"})
+       }
+       let user = new User({
+           email,password,name,
+       })
+       user = await user.save()
+       res.json(user)
+   }catch (e) {
+       res.status(500).json(
+           {
+               error:e.message
+           }
+       )
+   }
+
+
+
 })
 
 module.exports = {authRouter,};
