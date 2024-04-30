3469c6564efc8c0f291c75f5b07548c35d6b67fc
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-01 01:14:01 +0300

    serialized user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 class User {
   final String id;
   final String name;
+  final String email;
   final String password;
   final String address;
   final String type;
@@ -9,8 +12,24 @@ class User {
   User(
       {required this.id,
       required this.name,
+      required this.email,
       required this.password,
       required this.address,
       required this.type,
       required this.token});
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+        id: map['id'] ?? '',
+        name: map['name'] ?? '',
+        email: map['email'] ?? '',
+        password: map['password'] ?? '',
+        address: map['address'] ?? '',
+        type: map['type'] ?? '',
+        token: map['token'] ?? '');
+  }
+
+  String toJson() => json.encode(User);
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
 }
