40e35ab0ac7f4668448daf3a7f5ef226d87b4750
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-01 01:14:54 +0300

    added json serializer

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const mongoose =require("mongoose")
+mongoose.set('strictQuery', false);
+const path = require('path')
+const { authRouter } = require('./routes/auth')
 
 const app = express()
+app.use(express.json())
+
 const PORT = 3000;
 
-const { authRouter } = require('./routes/auth')
+mongoose.connect("mongodb://localhost:27017/")
+  .then(() => {
+    console.log("successfully connected to database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to database: ", error);
+  });
+
+
 app.use('/',authRouter)
+
 
 app.listen(PORT,"0.0.0.0",()=>{
    console.log(`connected to ${PORT}`)
