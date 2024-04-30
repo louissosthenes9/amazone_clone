5e431a63d0f58a48cd156a731e4357763f104401
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-01 01:12:25 +0300

    added text hint

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget{
+class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  const CustomTextField({super.key,required this.controller,required this.hintText});
-  
+  const CustomTextField(
+      {super.key, required this.controller, required this.hintText});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return TextFormField(
-      controller:TextEditingController(),
+      controller: TextEditingController(),
       decoration: InputDecoration(
+        hintText: hintText,
         border: const OutlineInputBorder(
-          borderSide:BorderSide(
-              color:Colors.black,
-          )
-        ),
+            borderSide: BorderSide(
+          color: Colors.black,
+        )),
         enabledBorder: const OutlineInputBorder(
-          borderSide:BorderSide(
-               color:Colors.black,
-         )
+            borderSide: BorderSide(
+          color: Colors.black,
+        )),
       ),
-    ),
-      validator:,
     );
   }
 }
