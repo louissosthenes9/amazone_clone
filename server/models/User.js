d9712a4c00a25a4025fd0012ee5abff14d7bef0c
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-19 21:43:16 +0300

    fixed the address bug

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-
+import mongoose from "mongoose";
+mongoose.set('strictQuery', true)
 const userSchema = mongoose.Schema(
     {
         name: {
@@ -13,7 +13,7 @@ const userSchema = mongoose.Schema(
             trim:true,
             validate:{
                 validator:(value)=>{
-                  const re = /^{s+6}/
+                  const re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/
                     return value.match(re);
                 },
 
@@ -35,7 +35,7 @@ const userSchema = mongoose.Schema(
         address : {
             type: String,
             default:'',
-            required: true,
+            required: false,
         },
 
         type:{
@@ -46,5 +46,5 @@ const userSchema = mongoose.Schema(
     }
 );
 
-const User = mongoose.model('User',userSchema);
-module.exports = User;
+let User = mongoose.model('User',userSchema);
+export default User 
